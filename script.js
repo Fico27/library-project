8ab57cdd3555cbd79ba800955fb8403c609eea1f
Phil <182430206+Fico27@users.noreply.github.com>
2025-03-16 12:28:53 +0100

    Added in 3 book objects to assist in building out book cards on the website.

--- script.js
+++ script.js
@@ -1,20 +1,51 @@
+const book1 = {
+    title: "The Name of the Wind",
+    author: "Patrick RothFuss",
+    genre: "Fantasy",
+    pages: 929, 
+    id: crypto.randomUUID()
+}
 
-const myLibrary = [];
+
+const book2 = {
+    title: "Doors of Stone",
+    author: "Patrick RothFuss",
+    genre: "Fantasy",
+    pages: 1000, 
+    id: crypto.randomUUID()
+}
+
+
+const book3 = {
+    title: "A Silent Regard to Slow Things",
+    author: "Patrick RothFuss",
+    genre: "Fantasy",
+    pages: 229, 
+    id: crypto.randomUUID()
+}
+
+
+
+const myLibrary = [book1,book2, book3];
 
 
 
 // the constructor
-function Book(title, author, genre){
-    this.title = title;
-    this.author= author;
-    this.genre = genre
-    this.id = crypto.randomUUID()
+function Book(title, author, genre, pages){
+    this.title = title,
+    this.author= author,
+    this.genre = genre,
+    this.pages = pages,
+    this.id = crypto.randomUUID();
 }
 
 // Takes params to make a new book using the book constructor
-function addBookToLibrary(title, author, genre) {
+function addBookToLibrary(title, author, genre, pages) {
 
     
-     myLibrary.push(new Book(title, author, genre));
+     myLibrary.push(new Book(title, author, genre, pages));
 
 }
+
+
+
